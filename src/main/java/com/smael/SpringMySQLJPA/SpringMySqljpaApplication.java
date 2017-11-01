f2e97a947b6173422a74934320d732b45795f3f4
Check <farid.m.check@gmail.com>
2017-11-01 16:17:54 +0100

    Ajout d'une fonction qui a permit que je puisse par la suite éxécuter des pages html

--- src/main/java/com/smael/SpringMySQLJPA/SpringMySqljpaApplication.java
+++ src/main/java/com/smael/SpringMySQLJPA/SpringMySqljpaApplication.java
@@ -1,7 +1,11 @@
 package com.smael.SpringMySQLJPA;
 
+import com.samskivert.mustache.Mustache;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mustache.MustacheEnvironmentCollector;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories( basePackages = "com.smael.SpringMySQLJPA.Repository")
@@ -12,4 +16,25 @@ public class SpringMySqljpaApplication {
         System.setProperty("spring.devtools.restart.enabled", "true");
 	    SpringApplication.run(SpringMySqljpaApplication.class, args);
 	}
+
+    /**
+     * Fonction qui a permit que je puisse executer des pages html.
+     * @param mustacheTemplateLoader
+     * @param environment
+     * @return
+     */
+    @Bean
+    public Mustache.Compiler mustacheCompiler(Mustache.TemplateLoader mustacheTemplateLoader,
+                                              Environment environment) {
+
+        MustacheEnvironmentCollector collector = new MustacheEnvironmentCollector();
+        collector.setEnvironment(environment);
+
+        // default value
+        Mustache.Compiler compiler = Mustache.compiler().defaultValue("")
+                .withLoader(mustacheTemplateLoader)
+                .withCollector(collector);
+        return compiler;
+
+    }
 }
