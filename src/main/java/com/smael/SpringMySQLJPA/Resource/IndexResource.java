c403e0f93635a62e4178b2d472f4f7f6fc8aaa51
Check <farid.m.check@gmail.com>
2017-11-02 11:34:09 +0100

    Formulaire avec méthode POST: accès de données du formulaire.

--- src/main/java/com/smael/SpringMySQLJPA/Resource/IndexResource.java
+++ src/main/java/com/smael/SpringMySQLJPA/Resource/IndexResource.java
@@ -1,25 +1,29 @@
 package com.smael.SpringMySQLJPA.Resource;
 
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @RestController(value ="/")
 @RequestMapping(value ="/")
 public class IndexResource {
 
-    @GetMapping(value="/")
-    public ModelAndView indexAction(ModelMap model){
-        ModelAndView mav = new ModelAndView();
-        mav.setViewName("index");
-        return mav;
-    }
-
-    @GetMapping(value="/login")
-    public ModelAndView loginAction(Model aModel){
+    @RequestMapping(value="/", method = GET)
+    public ModelAndView indexAction( Model model
+    ){
         ModelAndView mav = new ModelAndView();
         mav.setViewName("membre-form");
         return mav;
+    }
+
+    @RequestMapping(value = "/", method = POST)
+    public void postLogiForm(HttpServletRequest request){
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        System.out.println(">>>>>>>>>>>>>>>>>> USERNAME : "+username + " PASSWORD : "+password);
     }
 }
