fedb66997cc5d7525b63b199076505a03abba3ca
Check <farid.m.check@gmail.com>
2017-11-02 11:34:57 +0100

    Ajout de theme Bootstrap et mise en place du formulaire de login

--- src/main/java/com/smael/SpringMySQLJPA/SpringMySqljpaApplication.java
+++ src/main/java/com/smael/SpringMySQLJPA/SpringMySqljpaApplication.java
@@ -13,7 +13,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class SpringMySqljpaApplication {
 
 	public static void main(String[] args) {
-        System.setProperty("spring.devtools.restart.enabled", "true");
+       // System.setProperty("spring.devtools.restart.enabled", true);
 	    SpringApplication.run(SpringMySqljpaApplication.class, args);
 	}
 
---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,6 @@
+$(function($){
+    // Form login script
+    $('.message a').click(function(){
+        $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+    });
+}(jQuery));
