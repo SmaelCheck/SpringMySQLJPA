e38a8e6981048bcb83f96b6ca1fff5cd241ab42b
Check <farid.m.check@gmail.com>
2017-11-02 11:48:19 +0100

    Mise à jour de l'entité

--- src/main/java/com/smael/SpringMySQLJPA/Entity/Membre.java
+++ src/main/java/com/smael/SpringMySQLJPA/Entity/Membre.java
@@ -22,6 +22,15 @@ public class Membre {
     @Column(name="age", nullable = false)
     private int age;
 
+    @Column(name="username", nullable = false)
+    private String username;
+
+    @Column(name="email", nullable = false)
+    private String email;
+
+    @Column(name="password", nullable = false)
+    private String password;
+
     public Membre() {}
 
     public long getId() {
@@ -50,5 +59,29 @@ public class Membre {
 
     public void setAge(int age) {
         this.age = age;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
